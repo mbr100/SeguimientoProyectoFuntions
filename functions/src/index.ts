aad168c2d7a3280f7e333396a0f75dcc1d6335e9
Borregacion <mario.borrego100@gmail.com>
2024-07-26 00:23:57 +0200

    Seguimineto cloud funtions 1.0.1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
+import { onSchedule } from "firebase-functions/v2/scheduler";
+import { onDocumentUpdated, QueryDocumentSnapshot } from "firebase-functions/v2/firestore";
+import { firestore } from "firebase-admin";
+import { auth } from "firebase-functions";
+import { AuthEventContext, AuthUserRecord } from "firebase-functions/lib/common/providers/identity";
+import { Aviso } from "./aviso.model";
+
 import * as logger from "firebase-functions/logger";
 import admin = require('firebase-admin');
-import {onSchedule} from "firebase-functions/v2/scheduler";
-import {onDocumentUpdated, QueryDocumentSnapshot} from "firebase-functions/v2/firestore";
-import {firestore} from "firebase-admin";
 import DocumentData = firestore.DocumentData;
 import WriteResult = firestore.WriteResult;
 import QuerySnapshot = firestore.QuerySnapshot;
@@ -10,8 +14,8 @@ import QuerySnapshot = firestore.QuerySnapshot;
 admin.initializeApp();
 
 exports.crearAvisos = onSchedule("0 6 * * *", async () => {
-    const tramitesQuery = admin.firestore().collection("tramites").where("estado", "==", "ACTIVO");
-    const tramitesSnapshot = await tramitesQuery.get();
+    const tramitesQuery: firestore.Query<firestore.DocumentData> = admin.firestore().collection("tramites").where("estado", "==", "ACTIVO");
+    const tramitesSnapshot: firestore.QuerySnapshot<firestore.DocumentData> = await tramitesQuery.get();
 
     tramitesSnapshot.forEach( (doc:QueryDocumentSnapshot) => {
         const fechaActual: Date = new Date();
@@ -73,11 +77,108 @@ exports.eliminarAvisosDeTramitesEntregados = onDocumentUpdated('tramites/{tramit
     if (newData.estado === "Entregado" && oldData.estado !== "Entregado") {
         const tramiteCodigo = newData.codigo;
         await admin.firestore().collection("avisos").where("codigo", "==", tramiteCodigo).get()
-            .then( (querySnapshot: QuerySnapshot<firestore.DocumentData>) => {
+            .then((querySnapshot: QuerySnapshot<firestore.DocumentData>) => {
                 querySnapshot.forEach((doc: QueryDocumentSnapshot) => doc.ref.delete()
-                .then(_ => {logger.log("Aviso eliminado")})
-                .catch(() => logger.error("Error al eliminar el aviso")));
+                    .then(() => logger.log("Aviso eliminado"))
+                    .catch(() => logger.error("Error al eliminar el aviso"))
+                );
             });
-
     }
 });
+
+exports.crearNotificacionLogin = auth.user().beforeSignIn((user: AuthUserRecord, context: AuthEventContext) => {
+    logger.log("Creando notificacion de login");
+    const aviso: Aviso = new Aviso(user.displayName!, user.uid, user.email!,new Date(context.timestamp), context.ipAddress)
+    admin.firestore().collection("login").add(aviso)
+        .then(() => logger.log("Notificacion de login creada"))
+        .catch(() => logger.error("Error al crear la notificacion de login"));
+})
+
+
+
+
+
+//
+// const firestore = admin.firestore();
+//
+// exports.crearAvisos = onSchedule("0 6 * * *", async () => {
+//     const tramitesQuery = firestore.collection("tramites").where("estado", "in", ["ACTIVO", "Entregado"]);
+//     const tramitesSnapshot = await tramitesQuery.get();
+//
+//     const batch = firestore.batch();
+//
+//     tramitesSnapshot.forEach((doc) => {
+//         const fechaActual = new Date();
+//         const fechaDada = new Date(doc.data().fechaFinTramite);
+//         const diferenciaEnMilisegundos = fechaDada.getTime() - fechaActual.getTime();
+//         const diasRestantes = Math.ceil(diferenciaEnMilisegundos / (1000 * 60 * 60 * 24));
+//
+//         let mensaje;
+//         if (diasRestantes <= 9 && diasRestantes >= 0) {
+//             mensaje = "El tramite " + doc.data().codigo + " está por vencerse";
+//         } else if (diasRestantes < 0) {
+//             mensaje = "El tramite " + doc.data().codigo + " se ha vencido";
+//         }
+//
+//         if (mensaje) {
+//             const avisoRef = firestore.collection("avisos").doc();
+//             batch.set(avisoRef, {
+//                 fechaFinTrammite: doc.data().fechaFinTramite,
+//                 diasRestantes: diasRestantes,
+//                 mensaje: mensaje,
+//                 tipo: "TRAMITE",
+//                 codigo: doc.data().codigo,
+//                 idTramite: doc.id
+//             });
+//             logger.info("Aviso creado para el tramite ", doc.data().codigo);
+//         }
+//     });
+//
+//     await batch.commit();
+// });
+//
+// exports.eliminarAvisosEntregados = onSchedule("0 5 * * *", async () => {
+//     const tramitesQuery = firestore.collection("tramites").where("estado", "==", "Entregado");
+//     const tramitesSnapshot = await tramitesQuery.get();
+//
+//     const tramites = tramitesSnapshot.docs.map(doc => doc.data().codigo);
+//
+//     const avisosQuery = firestore.collection("avisos").where("codigo", "in", tramites);
+//     const avisosSnapshot = await avisosQuery.get();
+//
+//     const batch = firestore.batch();
+//
+//     avisosSnapshot.forEach((doc) => {
+//         batch.delete(doc.ref);
+//     });
+//
+//     await batch.commit();
+// });
+//
+// exports.eliminarAvisosDeTramitesEntregados = onDocumentUpdated('tramites/{tramiteId}', async (event) => {
+//     const newData = event.data!.after.data();
+//     const oldData = event.data!.before.data();
+//
+//     if (newData.estado === "Entregado" && oldData.estado !== "Entregado") {
+//         const tramiteCodigo = newData.codigo;
+//
+//         const avisosQuery = firestore.collection("avisos").where("codigo", "==", tramiteCodigo);
+//         const avisosSnapshot = await avisosQuery.get();
+//
+//         const batch = firestore.batch();
+//
+//         avisosSnapshot.forEach((doc) => {
+//             batch.delete(doc.ref);
+//         });
+//
+//         await batch.commit();
+//     }
+// });
+//
+// exports.crearNotificacionLogin = auth.user().beforeSignIn((user, context) => {
+//     logger.log("Creando notificacion de login");
+//     const aviso = new Aviso(user, new Date(), context.ipAddress);
+//     return firestore.collection("login").add(aviso)
+//         .then(() => logger.log("Notificacion de login creada"))
+//         .catch((error) => logger.error("Error al crear la notificacion de login", error));
+// });
